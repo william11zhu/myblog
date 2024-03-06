7e0be86b3498d867102e1c5ce2ed51f04d9e2ded
zhujj <william11zhu@qq.com>
2024-03-06 23:54:26 +0800

    fix: 修改网页标题栏logo

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,7 +5,7 @@ import { viteBundler } from '@vuepress/bundler-vite'
 export default defineUserConfig({
   base: '/myblog/',
   lang: 'zh-CN',
-  head: [['link', { rel: 'icon', href: '/images/icons/favicon-32x32.png' }]],
+  head: [['link', { rel: 'icon', href: '/myblog/images/icons/favicon-32x32.png' }]],
   title: '我的博客',
   description: '我的博客',
 
